a78decaf9e66b96b3552a80d037a340e517fcae7
Felipe Yglesias <glumpo.games@gmail.com>
2025-01-06 21:48:45 -0600

    Adding GitHub to upload

---
+++ Introd to programming/Methods in C#/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        double radius;
+        double circleArea;
+        double trapezoidheight;
+        double trapezoidwidth;
+        double trapezoidArea;
+        Console.WriteLine("Enter the radius of the circle: ");
+        radius = double.Parse(Console.ReadLine());
+        circleArea = CalculateCircleArea(radius);
+        Console.WriteLine("The result of the area is: " + circleArea);
+        Console.WriteLine("Enter the hight of the trapezoid: ");
+        trapezoidheight = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the width of the trapezoid: ");
+        trapezoidwidth = double.Parse(Console.ReadLine());
+        trapezoidArea = CalculateTrapezoidArea(trapezoidheight, trapezoidwidth);
+        Console.WriteLine("The area of the trapezoid is: " + trapezoidArea);
+    }
+    static double CalculateCircleArea(double radius)
+    {
+        double result = 0;
+        result = Math.PI * radius * radius;
+        return result;
+    }
+    static double CalculateTrapezoidArea(double height, double width)
+    {
+        double result = 0;
+        result += height * width;
+        result /= 2;
+        result *= height;
+        return result;
+    }
+}
---
+++ Introd to programming/Practical Application of Coding Concepts/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+public class Program
+{
+    public static void Main()
+    {
+        //Variable declaration
+        string task1 = "";
+        string task2 = "";
+        string task3 = "";
+
+        int taskCompleted;
+
+        bool taskCompleted1 = false;
+        bool taskCompleted2 = false;
+        bool taskCompleted3 = false;
+
+        do
+        {
+
+
+            //Ask for the task the user wants to add.
+            Console.WriteLine("Enter the task you want to add: ");
+
+            //Find if there is a task available and assign it.
+            if (task1 == "")
+            {
+               task1 = Console.ReadLine();
+            }
+            else if (task2 == "")
+            {
+                task2 = Console.ReadLine();
+            }
+            else if (task3 == "")
+            {
+                task3 = Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("There is no task field available");
+                break;
+            }
+        } while (true);
+        do
+        {
+            //Verify if there is a task to be completed.
+            if (taskCompleted1 == true &&  taskCompleted2 == true && taskCompleted3 == true)
+            {
+                Console.WriteLine("All the tasks are completed. Congratyulations!");
+                Console.WriteLine("1. " + task1 + "-- Completed --");
+                Console.WriteLine("2. " + task2 + "-- Completed --");
+                Console.WriteLine("3. " + task3 + "-- Completed --");
+                break;
+            }
+            else
+            {
+                //Display the pending tasks
+                Console.WriteLine("Please select the task to be completed 1, 2, or 3.");
+                if (taskCompleted1 == true)
+                {
+                    Console.WriteLine("1. " + task1 + "-- Completed --");
+                }
+                else
+                {
+                    Console.WriteLine("1. " + task1 + "-- Pending --");
+                }
+                if (taskCompleted2 == true)
+                {
+                    Console.WriteLine("2. " + task2 + "-- Completed --");
+                }
+                else
+                {
+                    Console.WriteLine("2. " + task2 + "-- Pending --");
+                }
+                if (taskCompleted3 == true)
+                {
+                    Console.WriteLine("3. " + task3 + "-- Completed --");
+                }
+                else
+                {
+                    Console.WriteLine("3. " + task3 + "-- Pending --");
+                }
+                //Ask for the task that wants to be completed.
+                taskCompleted = int.Parse(Console.ReadLine());
+                if (taskCompleted == 1 && taskCompleted1 == false)
+                {
+                    taskCompleted1 = true;
+                }
+                else if (taskCompleted == 2 && taskCompleted2 == false)
+                {
+                    taskCompleted2 = true;
+                }
+                else if (taskCompleted == 3 && taskCompleted3 == false)
+                {
+                    taskCompleted3 = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid task selected.");
+                }
+            }
+        } while (true);
+    }
+}
