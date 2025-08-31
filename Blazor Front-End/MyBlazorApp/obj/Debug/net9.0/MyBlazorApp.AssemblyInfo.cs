3f3216f6dc6b80893009e3ae0a90d945f0dfea30
Felipe Yglesias <glumpo.games@gmail.com>
2025-08-31 16:52:54 -0600

    Create project from scratch again
    Beginning the tutorial again to understand

--- Blazor Front-End/MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
+++ Blazor Front-End/MyBlazorApp/obj/Debug/net9.0/MyBlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+729c70f2ea843c819706dde29c8c550385575a15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cb2f51783f608f3d2aa89c9cfdae050b78874f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Blazor Front-End/MyBlazorApp/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ Blazor Front-End/MyBlazorApp/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5116,
+    interval: 5128,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5116
+    delay: 5128
   };
 
   /**
